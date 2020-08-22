0c09a18396641e95b99f9130c52103447429b568
Emmanuel Ma <emmanuelma@MacBook-Pro.local>
2020-08-22 19:22:44 -0400

    added backend

--- listRestaurants.php
+++ listRestaurants.php
@@ -18,24 +18,33 @@ include "php/config.php";
         <?php
         if (isset($_POST['restaurant-search-submit'])) {
             $name = trim(mysqli_real_escape_string($conn, $_POST['restaurant-search-name']));
- 
+
         
             if (empty($name)){
                 header('Location: ../search.php?search=empty');
             } else {
                 $searchCheck = "SELECT * FROM restaurant WHERE res_name='$name'";
                 $searchQuery = mysqli_query($conn, $searchCheck);
-                if (mysqli_num_rows($loginQuery) > 0) {
+                if (searchCheck > 0){
                     while ($row = mysqli_fetch_assoc($searchQuery)){
-
-                        echo $row['res_name']; 
+                        echo '<div class="card" style="width: 18rem;">
+                                <div class="card-body">
+                                    <h5 class="card-title">{$row["res_name"])</h5>
+                                    <h6 class="card-subtitle mb-2 text-muted">{$row["res_address"]}</h6>
+                                    <a href="#" class="card-link">Waiters</a>
+                                </div>
+                            </div>';
                     }
                 } else {
                     header('Location: ../search.php?search=none');
+                    quit();
+                    
                     
                 }
             }
+        }
         ?>
+
 
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
